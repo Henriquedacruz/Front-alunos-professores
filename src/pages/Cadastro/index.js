850a50d63562597b46ba6904e3fe793a245f62ee
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-10-07 08:53:18 -0300

    att cadastro usuario

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,42 +1,51 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Imagem from '../../components/Imagens/transparente.png';
-import api from '../../config/configApi.js'
-import '../../styles/cadastro.css';
+import { useState } from "react";
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import Imagem from '../../components/Imagens/transparente.png';
+import * as yup from "yup";
+import { ErrorMessage, Formik, Form, Field } from "formik";
+import Axios from "axios";
 
+function Index() {
 
+  const handleRegister = (values) => {
+    Axios.post("http://localhost:8080/register", {
+      email: values.email,
+      password: values.password,
+    }).then((response) => {
+      alert(response.data.msg);
+      console.log(response);
+    });
+  };
 
-function index() {
+  const validationsRegister = yup.object().shape({
+    email: yup
+      .string()
+      .email("email inválido")
+      .required("O email é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter pelo menos 8 caracteres")
+      .required("A senha é obrigatória"),
+    confirmation: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "As senhas são diferentes")
+      .required("A confirmação da senha é obrigatória"),
+  });
 
-    const [email, setEmail] = useState(''),
-    [password, setPassword] = useState(''),
-    [userName, setUserName] = useState('');
-
-    async function createUser() {
-        try {
-            await api.post('/user', { email, password, userName })
-
-            alert(`Usuario ${userName} cadastrado com sucesso`)
-        }
-        catch (err) {
-            alert(`Houve um erro: ${err}`)
-        }
-
-    }
+    
     return (
         <>
-            <div className='cadastro-cont'>
+            {/* <div className='cadastro-cont'>
                 <img className="cadastro-Logo" src={Imagem} />
                 <h3 className='tittle-cad'>Faça seu cadastro</h3>
                 <Form>
                     <div className='user-text'>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Usuário:</Form.Label>
-                            <Form.Control type="text" value={userName}
-      onChange={e => setUserName(e.target.value) } required />
+                            <Form.Control type="text" required />
                         </Form.Group>
-                        {/* <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Classe:</Form.Label>
                             <Form.Control type="text" required />
                         </Form.Group>
@@ -44,7 +53,7 @@ function index() {
                     <fieldset class="">
                         <div class="" >
                             <h6 class="">Tipo de conta:</h6>
-                            <div class="" className='cont-tipo'>
+                            <div class=""  className='cont-tipo'>
                                 <div class="">
                                     <input class="" type="radio" name="gridRadios" id="" value="opcao1" required></input>
                                     <label class="" for="gridRadios1">
@@ -73,24 +82,22 @@ function index() {
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Data de nascimento:</Form.Label>
-                            <Form.Control type="date" required />
-                        </Form.Group> */}
+                            <Form.Control type="date" required/>
+                        </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email:</Form.Label>
-                            <Form.Control type="email"  value={email} 
-       onChange={e => setEmail(e.target.value) }required />
+                            <Form.Control type="email" required/>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Senha:</Form.Label>
-                            <Form.Control type="password" value={password}
-      onChange={e => setPassword(e.target.value) } required />
+                            <Form.Control type="password" required/>
                         </Form.Group>
-                        {/* <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Telefone:</Form.Label>
-                            <Form.Control type="text" required />
-                        </Form.Group> */}
+                            <Form.Control type="text" required/>
+                        </Form.Group>
                     </div>
-                    {/* <fieldset class="">
+                    <fieldset class="" >
                         <div class="">
                             <h6 class="">Horário de curso:</h6>
                             <div class="" className='cont-tipo'>
@@ -114,18 +121,68 @@ function index() {
                                 </div>
                             </div>
                         </div>
-                    </fieldset> */}
+                    </fieldset>
                     <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check label=" Li e concordo com os termos de uso" id="box-check" />
+                        <Form.Check label=" Li e concordo com os termos de uso" id="box-check" required />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={createUser}>
+                    <Button variant="primary" type="submit">
                         Cadastrar-se
                     </Button>
                 </Form>
-            </div>
-        </>
-    )
+            </div> */}
+    <div className="container">
+   {/* /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+      <h1>Cadastro</h1>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleRegister}
+        validationSchema={validationsRegister}
+      >
+        <Form className="register-form">
+          <div className="register-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+
+          <div className="form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+
+          <div className="form-group">
+            <Field
+              name="confirmation"
+              className="form-field"
+              placeholder="Senha"
+            />
+
+            <ErrorMessage
+              component="span"
+              name="confirmation"
+              className="form-error"
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
     
+        </>
+    );
 }
 
-export default index
+export default Index
